c7f807156a452eb24d79e784f01569d3b6794bac
Dwi <98dwiyulianto@gmail.com>
2025-11-12 17:00:06 +0700

    fix: extend ImportMetaEnv typing to include DEV, PROD, MODE flags

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,13 @@
 // / <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE_URL: string;
+    readonly VITE_API_BASE_URL?: string;
+
+    // ✅ Built-in Vite environment flags
+    readonly MODE: string;
+    readonly DEV: boolean;
+    readonly PROD: boolean;
+    readonly SSR: boolean;
 }
 
 interface ImportMeta {
